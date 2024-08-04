6994850afe803904a7b21434e1b92806c27b7fff
Bleyn <bleynchannel@yandex.ru>
2024-08-04 15:11:04 +0400

    Change build.ps1

--- .deploy/build.ps1
+++ .deploy/build.ps1
@@ -8,5 +8,5 @@ if (-not (Test-Path -Path $PSScriptRoot\build\3d_detection.tar)) {
 	docker image save bleyn/3d_detection:latest -o $PSScriptRoot\build\3d_detection.tar
 }
 
-'docker load < $PSScriptRoot\3d_detection.tar
+'docker load --input $PSScriptRoot\3d_detection.tar
 docker-compose -f $PSScriptRoot\docker-compose.yml up -d' | Out-File -FilePath $PSScriptRoot\build\deploy.ps1
